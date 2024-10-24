e37da377b864e35d68ee62e43386497c92dc1226
Evlenium <evlenium@mail.ru>
2024-10-24 13:05:01 +0300

    Add secret token

--- app/src/main/java/com/practicum/movies/data/movies/network/IMDbApiService.kt
+++ app/src/main/java/com/practicum/movies/data/movies/network/IMDbApiService.kt
@@ -1,5 +1,6 @@
 package com.practicum.movies.data.movies.network
 
+import com.practicum.movies.BuildConfig
 import com.practicum.movies.data.cast.MovieCastResponse
 import com.practicum.movies.data.details.MovieDetailsResponse
 import com.practicum.movies.data.movies.dto.MoviesSearchResponse
@@ -8,16 +9,19 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface IMDbApiService {
-    @GET("/en/API/SearchMovie//{expression}")
+    @GET("/en/API/SearchMovie/$KEY/{expression}")
     suspend fun searchMovies(@Path("expression") expression: String): MoviesSearchResponse
 
-    @GET("/en/API/Title//{movie_id}")
+    @GET("/en/API/Title/$KEY/{movie_id}")
     suspend fun getMovieDetails(@Path("movie_id") movieId: String): MovieDetailsResponse
 
-    @GET("/en/API/FullCast//{movie_id}")
+    @GET("/en/API/FullCast/$KEY/{movie_id}")
     suspend fun getFullCast(@Path("movie_id") movieId: String): MovieCastResponse
 
-    @GET("/en/API/SearchName//{expression}")
+    @GET("/en/API/SearchName/$KEY/{expression}")
     suspend fun searchNames(@Path("expression") expression: String): NamesSearchResponse
 
+    companion object {
+        const val KEY = BuildConfig.GH_MOVIES_ACCESS_TOKEN
+    }
 }
